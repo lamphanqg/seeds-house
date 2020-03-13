9929ce14faa24fdd1bdfcfd2bcd9e9a998a9f220
Phan Quang Lam <p-lam@ruby-dev.jp>
2020-03-13 09:24:48 +0900

    Remove jsonbuilder

--- app/controllers/families_controller.rb
+++ app/controllers/families_controller.rb
@@ -2,13 +2,11 @@ class FamiliesController < ApplicationController
   before_action :set_family, only: [:show, :edit, :update, :destroy]
 
   # GET /families
-  # GET /families.json
   def index
     @families = Family.all
   end
 
   # GET /families/1
-  # GET /families/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class FamiliesController < ApplicationController
   end
 
   # POST /families
-  # POST /families.json
   def create
     @family = Family.new(family_params)
 
     respond_to do |format|
       if @family.save
         format.html { redirect_to @family, notice: 'Family was successfully created.' }
-        format.json { render :show, status: :created, location: @family }
       else
         format.html { render :new }
-        format.json { render json: @family.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /families/1
-  # PATCH/PUT /families/1.json
   def update
     respond_to do |format|
       if @family.update(family_params)
         format.html { redirect_to @family, notice: 'Family was successfully updated.' }
-        format.json { render :show, status: :ok, location: @family }
       else
         format.html { render :edit }
-        format.json { render json: @family.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /families/1
-  # DELETE /families/1.json
   def destroy
     @family.destroy
     respond_to do |format|
       format.html { redirect_to families_url, notice: 'Family was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
